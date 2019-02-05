81f9b9d0971e390c31af6192a8e799fb1261ba76
Hayk Karapetyan <hakarapet@gmail.com>
2019-02-05 22:01:33 +0100

    convert tabs to spaces

--- main.tf
+++ main.tf
@@ -1,16 +1,16 @@
 terraform {
-	backend "s3" {
-			encrypt         = true
-			bucket          = "hk-terraform-state-bucket"
-			key             = "hakarapet/terraform/lambda-playgound"
-			# role_arn        = "arn:aws:iam::529837707544:role/hk-terraform-role"
-			region          = "eu-central-1"
-			# dynamodb_table  = "hk-terraform-dynamodb-locks"
-		}
-	}
+  backend "s3" {
+      encrypt         = true
+      bucket          = "hk-terraform-state-bucket"
+      key             = "hakarapet/terraform/lambda-playgound"
+      # role_arn        = "arn:aws:iam::529837707544:role/hk-terraform-role"
+      region          = "eu-central-1"
+      # dynamodb_table  = "hk-terraform-dynamodb-locks"
+    }
+  }
 
-	provider "aws" {
-		region = "eu-central-1"
-		access_key = "${var.AWS_ACCESS_KEY_ID}"
-  	secret_key = "${var.SECRET_AWS_ACCESS_KEY}"
+  provider "aws" {
+    region = "eu-central-1"
+    access_key = "${var.AWS_ACCESS_KEY_ID}"
+    secret_key = "${var.SECRET_AWS_ACCESS_KEY}"
 }
