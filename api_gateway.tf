4289ed22490350ea564bfd2d88a3fa0e51913a4d
Hayk Karapetyan <hakarapet@gmail.com>
2019-02-11 20:24:18 +0100

    Updates the pathpart in api gateway resource. Updates the lambda file to return the request body

--- api_gateway.tf
+++ api_gateway.tf
@@ -1,12 +1,11 @@
 resource "aws_api_gateway_rest_api" "playground_api_gateway" {
   name        = "Playground Gateway"
-  description = "This is my API for demonstration purposes"
 }
 
 resource "aws_api_gateway_resource" "playground_api_gateway_resource" {
   rest_api_id = "${aws_api_gateway_rest_api.playground_api_gateway.id}"
   parent_id   = "${aws_api_gateway_rest_api.playground_api_gateway.root_resource_id}"
-  path_part   = "playgound"
+  path_part   = "playlambda"
 }
 
 resource "aws_api_gateway_method" "playground_api_gateway_method" {
--- playground_lambda.py
+++ playground_lambda.py
@@ -1,7 +1,10 @@
+import json
+import os
+
 def handler(event, context):
-  print('Hello From Inside of Lambda, bitch')
+  print(f'Hello From Inside of Lambda, the lambda variable is: {os.environ["lambda_variable"]}')
   # event['body']
   return {
     'statusCode': 200,
-    'body': 'Hello From Inside of Lambda, bitch'
+    'body': event['body']
   }
